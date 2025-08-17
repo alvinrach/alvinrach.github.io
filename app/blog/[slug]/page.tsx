d8ddf8d133948d8716e3d494c83f555552b6f9b8
alvinrach <alvinracht@gmail.com>
2025-08-17 23:15:48 +0700

    Next.js 15 things update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,12 +9,13 @@ export async function generateStaticParams() {
 }
 
 interface BlogPostPageProps {
-  params: {
+  params: Promise<{
     slug: string
-  }
+  }>
 }
 
-export default function BlogPostPage({ params }: BlogPostPageProps) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+  const { slug } = await params
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="max-w-7xl mx-auto p-4 lg:p-8">
@@ -26,7 +27,7 @@ export default function BlogPostPage({ params }: BlogPostPageProps) {
 
           {/* Blog Post Content */}
           <div className="lg:col-span-8">
-            <BlogPost slug={params.slug} />
+            <BlogPost slug={slug} />
           </div>
         </div>
       </div>
